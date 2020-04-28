c1d5a6bf0905af73ce374097eb762971ff41e338
Michael Monahan <mikey@jigsaw.xyz>
2020-04-27 23:40:38 -0400

    add more images

--- src/components/nav-button.js
+++ src/components/nav-button.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavButton = ({ title }) => {
+  const location = useLocation().pathname.replace('/', '');
   return (
     <Link to={`/${title}`} className="nav-button">
       <p
         className="nav-button__text"
         style={{
-          borderBottom: 'none',
+          borderBottom: title === location ? '1px solid black' : '1px solid transparent',
         }}
       >
         {title}
--- src/components/splash-image.js
+++ src/components/splash-image.js
@@ -4,9 +4,13 @@ import PastaOne from '../assets/header-images/pasta_one.jpg';
 import PastaTwo from '../assets/header-images/pasta_two.jpg';
 import ChickenOne from '../assets/header-images/chicken_one.jpg';
 import TacosOne from '../assets/header-images/tacos_one.jpg';
+import PizzaOne from '../assets/header-images/pizza_one.jpg';
+import CakeOne from '../assets/header-images/cake_one.jpg';
+import SaladOne from '../assets/header-images/salad_one.jpg';
+import SoupOne from '../assets/header-images/soup_one.jpg';
 
 const RandomizeImageDisplay = () => {
-  const potentialImages = [PastaOne, PastaTwo, ChickenOne, TacosOne];
+  const potentialImages = [SoupOne, PastaOne, PastaTwo, ChickenOne, TacosOne, PizzaOne, CakeOne, SaladOne];
   const randomImage = Math.floor(Math.random() * ((potentialImages.length) - 0)) + 0;
   const image = potentialImages[randomImage];
   return (
