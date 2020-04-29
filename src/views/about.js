f4016aacbc48c83efdf538d206b201af93dabb75
Michael Monahan <mikey@jigsaw.xyz>
2020-04-28 21:51:44 -0400

    simple package for fading in

--- src/views/about.js
+++ src/views/about.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 
 import text from '../constants/english.json';
 
@@ -11,57 +13,63 @@ const About = () => (
       <h4 className="about__quote">
         {text.about.quote}
       </h4>
-    <section className="about">
-      <div className="about__column left-column">
-        <img
-          className="about__column__image"
-          src={ChefOne}
-          alt="about-one"
-        />
-      </div>
-      <div className="about__column">
-        <h2 className="about__column__text-header">
-          {text.about.rowOneHeader}
-        </h2>
-        <p className="about__column__text">
-          {text.about.rowOne}
-        </p>
-      </div>
-    </section>
-    <section className="about reverse">
-      <div className="about__column left-column">
-        <h2 className="about__column__text-header">
-          {text.about.rowTwoHeader}
-        </h2>
-        <p className="about__column__text">
-          {text.about.rowTwo}
-        </p>
-      </div>
-      <div className="about__column">
-        <img
-          className="about__column__image"
-          src={KitchenOne}
-          alt="kitchen-one"
-        />
-      </div>
-    </section>
-    <section className="about">
-    <div className="about__column left-column">
-        <img
-          className="about__column__image"
-          src={CateringOne}
-          alt="catering-one"
-        />
-      </div>
-      <div className="about__column">
-        <h2 className="about__column__text-header">
-          {text.about.rowThreeHeader}
-        </h2>
-        <p className="about__column__text">
-          {text.about.rowThree}
-        </p>
-      </div>
-    </section>
+    <ScrollAnimation animateIn="fadeIn" animateOnce duration={2}>
+      <section className="about">
+        <div className="about__column left-column">
+          <img
+            className="about__column__image"
+            src={ChefOne}
+            alt="about-one"
+          />
+        </div>
+        <div className="about__column">
+          <h2 className="about__column__text-header">
+            {text.about.rowOneHeader}
+          </h2>
+          <p className="about__column__text">
+            {text.about.rowOne}
+          </p>
+        </div>
+      </section>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn="fadeIn" animateOnce duration={2}>
+      <section className="about reverse">
+        <div className="about__column left-column">
+          <h2 className="about__column__text-header">
+            {text.about.rowTwoHeader}
+          </h2>
+          <p className="about__column__text">
+            {text.about.rowTwo}
+          </p>
+        </div>
+        <div className="about__column">
+          <img
+            className="about__column__image"
+            src={KitchenOne}
+            alt="kitchen-one"
+          />
+        </div>
+      </section>
+    </ScrollAnimation>
+    <ScrollAnimation animateIn="fadeIn" animateOnce duration={2}>
+      <section className="about">
+        <div className="about__column left-column">
+          <img
+            className="about__column__image"
+            src={CateringOne}
+            alt="catering-one"
+          />
+        </div>
+        <div className="about__column">
+          <h2 className="about__column__text-header">
+            {text.about.rowThreeHeader}
+          </h2>
+          <p className="about__column__text">
+            {text.about.rowThree}
+          </p>
+        </div>
+      </section>
+    </ScrollAnimation>
   </div>
 );
 
