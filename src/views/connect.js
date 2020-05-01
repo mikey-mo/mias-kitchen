6dc08f7c39391ab67eb8a0bb8b263d7d4f7a8316
Michael Monahan <mikey@jigsaw.xyz>
2020-05-01 13:33:10 -0400

    add stuff to more stuff

--- src/views/connect.js
+++ src/views/connect.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import MailchimpSubscribe from "react-mailchimp-subscribe"
- 
+import InstagramEmbed from 'react-instagram-embed';
+import ScrollAnimation from 'react-animate-on-scroll';
+
 const url = "//xxxx.us13.list-manage.com/subscribe/post?u=zefzefzef&id=fnfgn";
 
 const Connect = () => (
   <div>
-    <MailchimpSubscribe url={url}/>
+    <ScrollAnimation
+      style={{
+        width: '80%',
+        margin: '0 auto',
+      }}
+      duration={1}
+      animateIn="fadeIn"
+    >
+      <MailchimpSubscribe url={url}/>
+    </ScrollAnimation>
+    <div style={{ padding: '20px 0', borderBottom: '1px solid black' }} />
+    <ScrollAnimation
+      style={{
+        width: '80%',
+        margin: '0 auto',
+      }}
+      duration={1}
+      animateIn="fadeIn"
+    >
+      <InstagramEmbed
+        url='https://instagr.am/p/Zw9o4/'
+        hideCaption
+        containerTagName='div'
+      />
+    </ScrollAnimation>
   </div>
 )
 
