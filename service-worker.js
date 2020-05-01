37740661bd3a0c39da0cc2ffcce28fb062c4f2b5
Michael Monahan <mikey@jigsaw.xyz>
2020-04-30 23:57:08 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mias-kitchen/precache-manifest.3370398ab5b1434b519518ff75f372b7.js"
+  "/mias-kitchen/precache-manifest.95f6d8da86f9a0e2ff2d7b9b65511272.js"
 );
 
 self.addEventListener('message', (event) => {
